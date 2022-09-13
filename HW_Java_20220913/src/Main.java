4e9e83763aefc2a7452cda2415bdfd3f9ef56ccd
Lera Marshal <marshalmarshal777@gmail.com>
2022-09-13 06:54:21 +0200

    Java 20220913

---
+++ HW_Java_20220913/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(replace("Masha", 'M', 'S'));
+    }
+
+    /*
+    Написать метод String replace(String str, char oldChar, char newChar),
+    который в переданной строке str меняет все символы  oldChar на символы newChar.
+    Например: replace(«саша», ‘c’, ‘м’) -> «маша»
+     */
+    private static String replace(String str, char oldChar, char newChar) {
+        String res = "";
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if (c == oldChar) {
+                res += newChar;
+            } else {
+                res += c;
+            }
+        }
+        return res;
+    }
+}
