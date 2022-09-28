584a29f0cd104ccb6f3b1dd0619407dc049f0f8a
Lera Marshal <marshalmarshal777@gmail.com>
2022-09-28 06:05:20 +0200

    Корректные комментарии

--- HW_Java_20220928_Tasks/src/Main.java
+++ HW_Java_20220928_Tasks/src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
-        /*
-        First level: Дан массив целых чисел (не пустой).
-        Написать метод, возвращающий наибольший элемент этого массива .
-
-        { 5,2,9,1} -> 9
-         */
-
         int[] arr = new int[] { 5, 2, 9, 1};
 
         System.out.println(findMaxElem(arr));
         System.out.println(getMaxMinDiff(arr));
     }
 
+
+    /*
+    First level: Дан массив целых чисел (не пустой).
+    Написать метод, возвращающий наибольший элемент этого массива .
+
+    { 5,2,9,1} -> 9
+     */
     private static int findMaxElem(int[] arr){
         int max = arr[0];
 
@@ -24,6 +23,12 @@ public class Main {
         return max;
     }
 
+    /*
+    Second level: Дан массив целых чисел ( не пустой).
+    Написать метод, возвращающий разницу между наибольшим и наименьшим  элементами  этого массива .
+
+    { 5,2,9,1} -> 8
+     */
     private static int getMaxMinDiff(int[] arr) {
         int min = arr[0];
         int max = arr[0];
