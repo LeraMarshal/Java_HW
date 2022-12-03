91bb3b4ffc08a8bbd1231fc26408789e0dd19458
Lera Marshal <marshalmarshal777@gmail.com>
2022-12-03 13:48:45 +0100

    Замеры производительности BubbleSort и QuickSort на рандомных массивах (1000, 10000, 100000, 1000000 элементов).

---
+++ HW_Algorithms_20221203/src/de/marshal/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package de.marshal.sort;
+
+public class BubbleSort {
+    public static long count = 0;
+
+    public static void bubbleSort(int[] arr) {
+        count = 0;
+
+        int len = arr.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j+1);
+                }
+
+                count += 1;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
